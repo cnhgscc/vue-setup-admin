c96f02d9015df8783c36756be4d7017454bb850d
austin <austin@fctx.com>
2024-01-11 17:30:29 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,21 @@
 <template>
-  <main>
-    大家好
-  </main>
+<main>
+  <el-tag>123132</el-tag>
+</main>
 </template>
 
 <script setup lang="ts">
+import {reactive} from 'vue'
+
+
+const vmps = reactive({
+})
+
 </script>
 <style scoped lang="scss">
+
+.el-tag{
+  font-size: 1rem;
+}
+
 </style>
